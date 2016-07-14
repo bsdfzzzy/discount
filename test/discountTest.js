ed824de8aeb784b86109877c8eb37641a0622bb3
bsdfzzzy <1002380934@qq.com>
2016-07-14 11:08:56 +0800

    test calculate finished with 3 success test

---
+++ test/discountTest.js
@@ -0,0 +1,7 @@
+const should = require('chai').should();
+const main = require('../dist/main.js');
+const Calculate = main.Calculate;
+
+/*describe('testing cashier', () => {
+
+});*/
--- test/tipTest.js
+++ test/tipTest.js
@@ -3,10 +3,27 @@ const main = require('../dist/main.js');
 const Calculate = main.Calculate;
 
 describe('testing calculate', () => {
-    describe('test calculate part', () => {
+    var c1 = new Calculate();
+    describe('test calculate part without discount', () => {
+
         it('it should be 9.0 when given 4.5 + 4.5', () => {
-            var c1 = new Calculate();
             c1.calculate(4.5, 4.5).should.to.eql(9.0);
         });
+
+        it('it should be 13.0 when given 4.5 + 4.5 + 4.0', () => {
+            c1.calculate(4.5, 4.5, 4.0).should.to.eql(13.0);
+        });
+
+        it('it should be 7.5 when given 1.5 + 2 + 3 + 1.0', () => {
+            c1.calculate(1.5, 2, 3, 1.0).should.to.eql(7.5);
+        });
+
     });
+
 });
+
+/*describe('testing print', () => {
+    describe('test print part without discount', () => {
+
+    });
+});*/
